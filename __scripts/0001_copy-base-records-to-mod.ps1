0e2fdaf18a1e59c972a3a2379f1c445c35905cd4
Solael <witchgod@solael.io>
2024-02-15 00:36:26 +0800

    Added controllers to mod.

--- __scripts/0001_copy-base-records-to-mod.ps1
+++ __scripts/0001_copy-base-records-to-mod.ps1
@@ -6,6 +6,23 @@ $source2 = "E:\SteamLibrary\steamapps\common\Grim Dawn\dev\mods\gdx1\database\re
 $source3 = "E:\SteamLibrary\steamapps\common\Grim Dawn\dev\mods\gdx2\database\records\skills"
 $destination = "E:\SteamLibrary\steamapps\common\Grim Dawn\dev\mods\ZeroOneFive\database\records\skills"
 
+if (-not (Test-Path -Path $destination)) {
+    New-Item -Path $destination -ItemType Directory -Force
+}
+
+Copy-Item -Path "$source1\*" -Destination $destination -Recurse -Force -ErrorAction SilentlyContinue
+Copy-Item -Path "$source2\*" -Destination $destination -Recurse -Force -ErrorAction SilentlyContinue
+Copy-Item -Path "$source3\*" -Destination $destination -Recurse -Force -ErrorAction SilentlyContinue
+
+$source1 = "E:\SteamLibrary\steamapps\common\Grim Dawn\dev\database\records\controllers"
+$source2 = "E:\SteamLibrary\steamapps\common\Grim Dawn\dev\mods\gdx1\database\records\controllers"
+$source3 = "E:\SteamLibrary\steamapps\common\Grim Dawn\dev\mods\gdx2\database\records\controllers"
+$destination = "E:\SteamLibrary\steamapps\common\Grim Dawn\dev\mods\ZeroOneFive\database\records\controllers"
+
+if (-not (Test-Path -Path $destination)) {
+    New-Item -Path $destination -ItemType Directory -Force
+}
+
 Copy-Item -Path "$source1\*" -Destination $destination -Recurse -Force -ErrorAction SilentlyContinue
 Copy-Item -Path "$source2\*" -Destination $destination -Recurse -Force -ErrorAction SilentlyContinue
 Copy-Item -Path "$source3\*" -Destination $destination -Recurse -Force -ErrorAction SilentlyContinue
